2b477b080abc15ba98d8c799bb13315221861e57
mark <m.samojlenko@autodrive-agency.ru>
2022-11-11 21:52:28 +0300

    инициализация вебпака, создание папок для стилей и для js

---
+++ public/js/index.js
@@ -0,0 +1,104 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/components/_hello.js":
+/*!*******************************************!*\
+  !*** ./resources/js/components/_hello.js ***!
+  \*******************************************/
+/***/ (() => {
+
+eval("console.log('hello world');//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9yZXNvdXJjZXMvanMvY29tcG9uZW50cy9faGVsbG8uanMuanMiLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9yZXNvdXJjZXMvanMvY29tcG9uZW50cy9faGVsbG8uanM/MmE2MyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaGVsbG8gd29ybGQnKTtcbiJdLCJtYXBwaW5ncyI6IkFBQUFBLE9BQU8sQ0FBQ0MsR0FBRyxDQUFDLGFBQWEsQ0FBQyJ9\n//# sourceURL=webpack-internal:///./resources/js/components/_hello.js\n");
+
+/***/ }),
+
+/***/ "./resources/js/index.js":
+/*!*******************************!*\
+  !*** ./resources/js/index.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("var req = __webpack_require__(\"./resources/js/components sync recursive _[^_].*\\\\.(js)$\");\nreq.keys().forEach(function (key) {\n  return req(key);\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9yZXNvdXJjZXMvanMvaW5kZXguanMuanMiLCJtYXBwaW5ncyI6IkFBQUEsSUFBTUEsR0FBRyxHQUFHQywrRUFBdUQ7QUFDbkVELEdBQUcsQ0FBQ0csSUFBSSxFQUFFLENBQUNDLE9BQU8sQ0FBQyxVQUFBQyxHQUFHO0VBQUEsT0FBSUwsR0FBRyxDQUFDSyxHQUFHLENBQUM7QUFBQSxFQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcmVzb3VyY2VzL2pzL2luZGV4LmpzP2JmN2UiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgcmVxID0gcmVxdWlyZS5jb250ZXh0KFwiLi9jb21wb25lbnRzXCIsIHRydWUsIC9fW15fXS4qXFwuKGpzKSQvKTtcbnJlcS5rZXlzKCkuZm9yRWFjaChrZXkgPT4gcmVxKGtleSkpO1xuIl0sIm5hbWVzIjpbInJlcSIsInJlcXVpcmUiLCJjb250ZXh0Iiwia2V5cyIsImZvckVhY2giLCJrZXkiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./resources/js/index.js\n");
+
+/***/ }),
+
+/***/ "./resources/js/components sync recursive _[^_].*\\.(js)$":
+/*!******************************************************!*\
+  !*** ./resources/js/components/ sync _[^_].*\.(js)$ ***!
+  \******************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+var map = {
+	"./_hello.js": "./resources/js/components/_hello.js"
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	if(!__webpack_require__.o(map, req)) {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return map[req];
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = "./resources/js/components sync recursive _[^_].*\\.(js)$";
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./resources/js/index.js");
+/******/ 	
+/******/ })()
+;
