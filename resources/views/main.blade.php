52de3c789cc8901c40ff6e34b9ae1ad34f00b256
Gifesv <egor3387801@yandex.ru>
2022-11-20 14:00:16 +0300

    Сделал главную страницу

---
+++ resources/views/main.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/index.css">
+    <title>Можно</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header__inner">
+                <div class="header__logo">
+                    <img class="header__img" src="public/img/header/logo.svg" alt="logo" />
+                    <div class="header__agency">BRANDING <br /> AGENCY</div>
+                </div>
+                 <nav class="header__nav">
+                    <ul class="header__ul">
+                        <li><a class="header__link" href="#">о нас</a></li>
+                        <li><a class="header__link" href="#">портфолио</a></li>
+                        <li><a class="header__link" href="#">услуги</a></li>
+                        <li><a class="header__link" href="#">блог</a></li>
+                        <li><a class="header__link" href="#">аудит</a></li>
+                        <li><a class="header__link header__contact" href="#">связаться</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <div class="tagline">
+        <div class="tagline__container">
+            <p class="tagline__text">Мы создаем бренды, <br> которыми предприниматели <br> будут гордиться</p>
+            <img src="public/img/main/one_letter_logo.png" alt="one_letter_logo">
+        </div>
+    
+    </div>
+    <div class="list">
+        <ul class="list__items">
+            <li class="list__item">упаковка бренда</li>
+            <div class="list__line"></div>
+            <li class="list__item">дизайн</li>
+            <div class="list__line"></div>
+            <li class="list__item">исследования</li>
+            <div class="list__line"></div>
+            <li class="list__item">таргет</li>
+            <div class="list__line"></div>
+            <li class="list__item">соцсети</li>
+            <div class="list__line"></div>
+            <li class="list__item">исследования</li>
+            <div class="list__line"></div>
+            <li class="list__item">сайты</li>
+        </ul>
+    </div>
+    <div class="projects">
+        <div class="line "></div>
+        <div class="asd">
+            <div class="qwe">
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+                <p class="projects__text">ПРОЕКТЫ АГЕНСТВА</p>
+            </div>
+        </div>
+       
+        <!-- <p class="projects__text">ПРОЕКТЫ АГЕНСТВА ПРОЕКТЫ АГЕНСТВА ПРОЕКТЫ АГЕНСТВА ПРОЕКТЫ АГЕНСТВА ПРОЕКТЫ АГЕНСТВА ПРОЕКТЫ АГЕНСТВА </p> -->
+        <div class="line "></div>
+        <div class="projects__images">
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/pika_coffee.png" alt="pika_coffee">
+                <p class="projects__image_text">Айдентика и дизайн <br> этикеток для Pika <br> Coffee</p>
+            </div>
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/beirut.png" alt="beirut">
+                <p class="projects__image_text">Айдентика для <br> закусочной Бейрут</p>
+            </div>
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/tiger_stickers.png" alt="tiger_stickers">
+                <p class="projects__image_text">Стикерпак «Тигр» <br> для газеты <br> «Известия»</p>
+            </div>
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/merch.png" alt="merch">
+                <p class="projects__image_text">Иллюстрации и <br> мерч</p>
+            </div>
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/health.png" alt="health">
+                <p class="projects__image_text">Постеры с услугами <br> для института <br> здоровья</p>
+            </div>
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/dragon_stickers.png" alt="dragon_stickers">
+                <p class="projects__image_text">Стикерпак «Дракон» <br> для газеты <br> «Известия»</p>
+            </div>
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/bom_blue.png" alt="bom_blue">
+                <p class="projects__image_text">Логотип и наклейки <br> на упаковку для <br> Bom Blue</p>
+            </div>
+            <div class="projects__image">
+                <img class="projects__image_width" src="public/img/main/algorithm_club.png" alt="algorithm_club">
+                <p class="projects__image_text">Брендинг и соцсети <br> для Alorithm <br> Club</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="about_projects">
+        <p class="about_projects__text about_projects__text1">Каждому проекту мы создаем сильную брендинговую платформу, чтобы креативные идеи работали на ваш бизнес.</p>
+        <p class="about_projects__text">Мы знаем, как для предпринимателя важно его дело, поэтому выстраиваем нашу работу понятно и комфортно для всех участников.</p>
+        <button class="about_projects__button">НАШИ УСЛУГИ</button>
+    </div>
+    <div class="team">
+        <div class="line"></div>
+        <p class="projects__text">КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА КОМАНДА </p>
+        <div class="line"></div>
+        <div class="team__members">
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+            <div class="team__image">
+                <img class="team__image_pos" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team__text">Язагит Лера <br> <span>дизайнер</span></p> 
+            </div>
+        </div>
+    </div>
+    <div class="contact">
+        <div class="line"></div>
+        <p class="projects__text">СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ СВЯЗАТЬСЯ  </p>
+        <div class="line"></div>
+        <div class="contact__block">
+            <p class="contact__text">Здесь можно задать вопрос <br> или оформить заказ</p>
+            <form action="" class="contact__form">
+                <input class="contact__form_name contact__form_view" type="text" placeholder="Имя*">
+                <input class="contact__form_phone contact__form_view" type="text" placeholder="Телефон*">
+                <input class="contact__form_email contact__form_view" type="text" placeholder="E-mail*">
+                <textarea class="contact__form_mail contact__form_view" placeholder="Сообщение"></textarea>
+                <input class="contact__form_submit" type="submit">
+            </form>
+        </div>
+    </div>
+    <div class="line"></div>
+    <p class="projects__text">КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ КОНТАКТЫ </p>
+    <div class="line"></div>
+    
+    <div class="footer">
+        <div class="footer__container">
+            <div>
+                <p class="footer__text footer__text1">Телефон: +7 (495) 000 00 00</p>
+                <p class="footer__text">E-mail: mozhno@agency.com</p>
+            </div>
+            <div>
+                <img class="footer__logo" src="public/img/footer/be_logo.svg" alt="">
+                <img src="public/img/footer/tg_logo.svg" alt="">
+            </div>
+        </div>
+       
+
+    </div>
+</body>
+
+</html>
