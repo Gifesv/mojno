1dad0aa5727bdf63bda5f1746a2fbd8ae24290fc
Gifesv <egor3387801@yandex.ru>
2022-11-23 11:54:31 +0300

    сделал компоненты

---
+++ resources/views/components/contact.blade.php
@@ -0,0 +1,33 @@
+<div class="contact">
+        <div class="section-title">
+            <div class="section-title__line">
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
+                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>  
+            </div>
+        </div>
+        <div class="contact__wrap">
+            <p class="contact__text">Здесь можно задать вопрос <br> или оформить заказ</p>
+            <form action="" class="form">
+                <input class="form__input" type="text" placeholder="Имя*" name="name">
+                <input class="form__input" type="text" placeholder="Телефон*" name="phone">
+                <input class="form__input" type="text" placeholder="E-mail*" name="email">
+                <textarea class="form__textarea form__input" placeholder="Сообщение" name="mail"></textarea>
+                <input class="form__btn" type="submit" name="btn">
+            </form>
+        </div>
+    </div>
---
+++ resources/views/components/team.blade.php
@@ -0,0 +1,59 @@
+<div class="team">
+        <div class="section-title">
+            <div class="section-title__line">
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+                <p class="section-title__line-text">КОМАНДА</p>
+            </div>
+        </div>
+        <div class="team-list">
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+            <div class="team-list__item">
+                <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+                <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+            </div>
+        </div>
+    </div>
