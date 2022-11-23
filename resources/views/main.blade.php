9125c0bac5530c7be6a3c85e6ac1274d58b5acfa
Gifesv <egor3387801@yandex.ru>
2022-11-23 12:11:12 +0300

    правки

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -119,67 +119,7 @@
 
     <x-team></x-team>
 
-<<<<<<< HEAD
-    <div class="contact">
-        <div class="section-title">
-            <div class="section-title__line">
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>
-                <p class="section-title__line-text">СВЯЗАТЬСЯ</p>  
-            </div>
-        </div>
-        <div class="contact__wrap">
-            <p class="contact__text">Здесь можно задать вопрос <br> или оформить заказ</p>
-            <form action="" class="form">
-                <input class="form__input" type="text" placeholder="Имя*" name="name">
-                <input class="form__input" type="text" placeholder="Телефон*" name="phone">
-                <input class="form__input" type="text" placeholder="E-mail*" name="email">
-                <textarea class="form__textarea form__input" placeholder="Сообщение" name="mail"></textarea>
-                <input class="form__btn" type="submit" placeholder="Отправить" name="btn">
-            </form>
-        </div>
-    </div>
-
-    <div class="section-title">
-        <div class="section-title__line">
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            <p class="section-title__line-text">КОНТАКТЫ</p>
-            
-        </div>
-    </div>
-=======
     <x-contact></x-contact>
->>>>>>> about-page
 
     <div class="footer">
         <div class="section-title">
