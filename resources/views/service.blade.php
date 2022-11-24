18cc3f131c46853224c64826e8678355fd3377e3
Gifesv <egor3387801@yandex.ru>
2022-11-24 23:15:53 +0300

    почти сделал страницу услуги(надо допилить)

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -22,10 +22,7 @@
             <p class="section-title__line-text">УСЛУГИ</p>
         </div>
     </div>
-    <div class="service__logo">
-        <img class="service__img" src="/public/img/services/services-logo.png" alt="">
-    </div>
-
+    <img class="service__img" src="/public/img/services/services-logo.png" alt="services-logo">
     <div class="service__content">
         <h2 class="service__title">МОЖНО:</h2>
         <div class="service__wrap">
@@ -36,11 +33,171 @@
             <p class="service__subtitle">разработать дизайн</p>
             <p class="service__subtitle">создать сайт</p>
         </div>
-        <p class="service__text">Мы знаем, что у каждой компании есть разные нужды: кому-то нужен сайт, кому-то упаковка. Поэтому мы сделали
+        <p class="service__text">Мы знаем, что у каждой компании есть разные нужды: кому-то нужен сайт, кому-то
+            упаковка. Поэтому мы сделали
             базу к которой вы можете добавить любую из наших услуг уже после того, как мы разберемся с тем, какие шаги
-            стоит предпринимать вашему бренду дальше.</p> 
+            стоит предпринимать вашему бренду дальше.</p>
     </div>
 
+</div>
+
+<div class="offers">
+    <div class="section-title">
+        <div class="section-title__line">
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+            <p class="section-title__line-text">ПАКЕТ УСЛУГ</p>
+        </div>
+    </div>
+    <img class="offers__img" src="public/img/services/offers-img.png" alt="offers-img">
+    <div class="offers__content">
+        <div class="offers__wrap">
+            <h2 class="offers__title">БАЗОВЫЙ ПАКЕТ УСЛУГ</h2>
+            <h2 class="offers__price">89 000 ₽</h2>
+            <div class="offers__line"></div>
+            <h2 class="offers__title">ЧТО ВХОДИТ В ПАКЕТ</h2>
+            <div class="offers__item">
+                <h2 class="offers__title">АУДИТ</h2>
+                <img src="public/img/services/plus.png" alt="plus">
+            </div>
+            <div class="offers__item">
+                <h2 class="offers__title">ПОЗИЦИОНИРОВАНИЕ + КРЕАТИВНАЯ СТРАТЕГИЯ</h2>
+                <img src="public/img/services/minus.png" alt="minus">
+            </div>
+            <div class="offers__item-content">
+                <h3 class="offers__item-title">Что входит в позиционирование:</h3>
+                <ul class="offers__list">
+                    <li class="offers__list-item">исследование и анализ</li>
+                    <li class="offers__list-item">креативная стратегия</li>
+                    <li class="offers__list-item">сущность бренда</li>
+                    <li class="offers__list-item">рациональные преимущества</li>
+                    <li class="offers__list-item">эмоциональные преимущества</li>
+                    <li class="offers__list-item">идея позиционирования</li>
+                    <li class="offers__list-item">ценности</li>
+                    <li class="offers__list-item">характер</li>
+                    <li class="offers__list-item">видение</li>
+                    <li class="offers__list-item">миссия</li>
+                    <li class="offers__list-item">визуальные поиски</li>
+                </ul>
+            </div>
+            <div class="offers__item">
+                <h2 class="offers__title">ФИРМЕННЫЙ СТИЛЬ</h2>
+                <img src="public/img/services/plus.png" alt="plus">
+            </div>
+            <div class="offers__line"></div>
+            <h2 class="offers__subtitle">Все вышеперечисленное мы собираем в <span>брендбук*</span></h2>
+            <p class="offers__text"><span>*брендбук</span> – детальное руководство по использованию фирменного стиля
+                <br> и маркетинговой платформы в виде презентации
+            </p>
+            <button class="offers__btn">Связаться с нами</button>
+        </div>
+        <p class="offers__content-text">*до конца сентября действует скидка 20%</p>
+    </div>
+</div>
+
+<div class="products">
+    <div class="section-title">
+        <div class="section-title__line">
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+            <p class="section-title__line-text">ЗАКАЗАТЬ ОТДЕЛЬНО</p>
+        </div>
+    </div>
+
+    <div class="products__content">
+        <h2 class="products__title">УСЛУГИ ПО ДИЗАЙНУ <span>(без брендбука)</span> :</h2>
+        <div class="products__wrap">
+            <div>
+                <p class="products__text">POS–материалы <span>(бланки, флаеры, плакаты и другие)</span></p>
+                <p class="products__text">Упаковка <span>(этикетка)</span> </p>
+                <p class="products__text">Сайт <span>(лендинг)</span> </p>
+                <p class="products__text">Сайт <span>(многостраничный)</span> </p>
+                <p class="products__text">Стикерпак <span>(20 шт)</span> </p>
+                <p class="products__text">Иллюстрация <span>(1 шт)</span> </p>
+            </div>
+            <div>
+                <p class="products__price">| &nbsp 7 000 ₽/шт</p>
+                <p class="products__price">| &nbsp 10 000 ₽/шт</p>
+                <p class="products__price">| &nbsp <span>от</span> 40 000 ₽</p>
+                <p class="products__price">| &nbsp <span>от</span> 80 000 ₽</p>
+                <p class="products__price">| &nbsp 30 000 ₽</p>
+                <p class="products__price">| &nbsp <span>от</span> 10 000 ₽</p>
+            </div>
+        </div>
+        <div class="offers__line"></div>
+        <h2 class="products__title">КАК ПРОХОДИТ РАБОТА НАД ПРОЕКТОМ</h2>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo1.png" alt="logo1">
+            <p class="products__item-text">Обсуждение задачи с клиентом, сроки, бюджет <br>
+                <span>(можно через бота в телеграмме или на сайте)</span>
+            </p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo2.png" alt="logo2">
+            <p class="products__item-text">Подписание договора</p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo3.png" alt="logo3">
+            <p class="products__item-text">Внос 50% предоплаты</p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo4.png" alt="logo4">
+            <p class="products__item-text">Начало работы<br>
+                <span>(по этапам)</span>
+            </p>
+        </div>
+        <div class="offers__line"></div>
+        <h2 class="products__title">ЭТАПЫ РАБОТЫ НАД БРЕНДОМ</h2>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo5.png" alt="logo5">
+            <p class="products__item-text">Аудит с дизайнером, маркетологом и СММ-специалистом <br>
+                <span>(2-3 часа)</span>
+            </p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo6.png" alt="logo6">
+            <p class="products__item-text">1-2 недели готовим исследование <span>(концепция <br> позиционирования и
+                    креативная стратегия, визуальные <br> поиски)</span></p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo7.png" alt="logo7">
+            <p class="products__item-text">Презентуем 2-3 варианта лого с визуализацией, после <br> чего вы выбираете  один из представленных вариантов
+            </p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo8.png" alt="logo8">
+            <p class="products__item-text">Разрабатываем дизайн носителей
+            </p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo9.png" alt="logo9">
+            <p class="products__item-text">Дополнительные услуги <span>(если вы что-то добавляли <br> в пакет)</span> 
+            </p>
+        </div>
+        <div class="products__item">
+            <img class="products__img" src="public/img/services/logo10.png" alt="logo10">
+            <p class="products__item-text">Собираем брендбук с маркетинговой платформой <br> и фирменным стилем
+            </p>
+        </div>
+    </div>
 </div>
 
 @endsection
