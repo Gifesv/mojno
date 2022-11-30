543f2dacfc843ae578c549bb4a2ef6a94fb97fc4
Marinad <samojlenko.wot@gmail.com>
2022-11-30 22:06:42 +0300

    переделал слайдер из-за бага при переходе на десктоп

--- resources/js/components/_team.js
+++ resources/js/components/_team.js
@@ -1,53 +1,42 @@
-let slider = false;
 $(document).ready(function(){
-    teamSlider();
-    $(window).on('resize', function(){
-        teamSlider();
+    $(".team-list_sm").slick({
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20%',
+        slidesToShow: 1,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 650,
+                settings: {
+                    centerPadding: '20%',
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    centerPadding: '15%',
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                    centerPadding: '10%',
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    centerPadding: '7%',
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    centerPadding: '0',
+                }
+            },
+        ]
     });
 });
 
-function teamSlider(){
-    if (window.matchMedia('(max-width: 768px)').matches) {
-        if (!slider) {
-            $(".team-list").slick({
-                arrows: false,
-                centerMode: true,
-                centerPadding: '150px',
-                slidesToShow: 1,
-                infinite: true,
-                responsive: [
-                    {
-                        breakpoint: 650,
-                        settings: {
-                            centerPadding: '100px',
-                        }
-                    },
-                    {
-                        breakpoint: 550,
-                        settings: {
-                            centerPadding: '50px',
-                        }
-                    },
-                    {
-                        breakpoint: 450,
-                        settings: {
-                            centerPadding: '25px',
-                        }
-                    },
-                    {
-                        breakpoint: 375,
-                        settings: {
-                            centerPadding: '0',
-                        }
-                    },
-                ]
-            });
-            slider = true;
-        }
-    } else if (!window.matchMedia('(max-width: 768px)').matches) {
-        if (slider) {
-            $('.team-list').slick('unslick');
-            slider = false;
-        }
-    }
-};
---
+++ resources/views/components/team-list.blade.php
@@ -0,0 +1,32 @@
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
+<div class="team-list__item">
+    <img class="team-list__item-img" src="public/img/main/party_member.png" alt="party_member">
+    <p class="team-list__item-text">Язагит Лера <br> <span>дизайнер</span></p>
+</div>
