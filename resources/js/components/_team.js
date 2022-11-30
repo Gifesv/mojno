a1b58330292a60d0ea160578a1df56762ea61abd
Marinad <samojlenko.wot@gmail.com>
2022-11-30 17:00:12 +0300

    починил слайдер

--- resources/js/components/_team.js
+++ resources/js/components/_team.js
@@ -1,20 +1,36 @@
-$(document).ready(function() {
-   $('.team-list').slick({
-       responsive: [
-           {
-             breakpoint: 9999,
-             settings: 'unslick',
-           },
-           {
-               breakpoint: 768,
-               settings: {
-                   arrows: false,
-                   centerMode: true,
-                   centerPadding: '150px',
-                   slidesToShow: 1,
-                   infinite: true,
-               }
-           },
-       ],
-   });
+let slider = false;
+$(document).ready(function(){
+    teamSlider();
+    $(window).on('resize', function(){
+        teamSlider();
+    });
 });
+
+function teamSlider(){
+    if($(window).width() <= 768){
+        if(!slider){
+            console.log('resp')
+            $(".team-list").slick({
+                arrows: false,
+                centerMode: true,
+                centerPadding: '150px',
+                slidesToShow: 1,
+                infinite: true,
+                responsive: [
+                    {
+                        breakpoint: 1,
+                        settings: {
+                            centerPadding: '100px',
+                        },
+                    }
+                ]
+            });
+            slider = true;
+        }
+    } else if($(window).width() > 768){
+        if(slider){
+            $('.team-list').slick('unslick');
+            slider = false;
+        }
+    }
+};
